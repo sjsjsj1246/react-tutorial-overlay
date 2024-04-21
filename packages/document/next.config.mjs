3a6fd623c9031bf5fb558c374928e7e45c39303f
sjsjsj1246 <sjsjsj1246@gmail.com>
2024-04-21 16:26:43 +0900

    fix: document site rewrite paths

--- packages/document/next.config.mjs
+++ packages/document/next.config.mjs
@@ -6,6 +6,7 @@ import createMDX from '@next/mdx'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   output: 'export',
+  trailingSlash: true
 }
  
 const withMDX = createMDX({
